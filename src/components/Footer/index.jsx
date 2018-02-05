13085f1aac29b8a51f5d3ca519839fc21e36989f
volcanoliuc <liucannn@sina.com>
2018-02-05 18:08:49 +0800

    修改footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,10 +4,18 @@ import './style.less'
 const Footer = () => (
     <div className="footer">
         <ul className="footer-wrap">
-            <li className="footer-item active">首页</li>
-            <li className="footer-item">发现</li>
-            <li className="footer-item">订单</li>
-            <li className="footer-item">我的</li>
+            <li className="footer-item active">
+                <span>首页</span>
+            </li>
+            <li className="footer-item">
+                <span>发现</span>
+            </li>
+            <li className="footer-item">
+                <span>订单</span>
+            </li>
+            <li className="footer-item">
+                <span>我的</span>
+            </li>
         </ul>
     </div>
 )
