201770c6556979126a171526dbf2d7dc58d2029e
liucannn@sina.com <liucannn@sina.com>
2018-03-05 23:02:03 +0800

    activitylist

--- src/components/ShopActivity/index.jsx
+++ src/components/ShopActivity/index.jsx
@@ -12,33 +12,42 @@ class ShopActivity extends React.Component{
     }
     
     render(){
-        const activityList = [1,2,3,4,5]
+        const activityList = this.props.activities
         const showMore = this.state.showMore
+        const recommend = this.props.recommend
         return (
             <div className="shop-activity">
                 <div className="activity-left"></div>
                 <div className="activity-right">
                     <div className="tagLine">
-                        <img src="http://fuss10.elemecdn.com/a/c1/24c767ffa7fd296d3e2d6f01798c6png.png?imageMogr/format/webp/thumbnail/!60x60r/gravity/Center/crop/60x60/" alt="tag"/>
-                        <span>口碑人气好店</span>
+                        {!recommend.is_ad && recommend.reason
+                        ?<div>
+                            <img src="http://fuss10.elemecdn.com/a/c1/24c767ffa7fd296d3e2d6f01798c6png.png?imageMogr/format/webp/thumbnail/!60x60r/gravity/Center/crop/60x60/" alt="tag"/>
+                            <span>{recommend.reason}</span>
+                        </div>
+                        :""}   
                     </div>
                     <div className="activities">
                         <div className="activity-list">
-                            <div>
-                                {showMore?
-                                    activityList.map((val,index)=>(
-                                        <div key={index}>活动</div>
-                                    ))
-                                    :activityList.slice(0,2).map((val,index)=>(
-                                        <div key={index}>活动</div>
-                                    ))
-                                }
-                            </div>
+                            {showMore?
+                                activityList.map((val,index)=>(
+                                    <div className="shop-activity-item" key={index}>
+                                        <span className="activity-icon" style={{background:`#${val.icon_color}`}}>{val.icon_name}</span>
+                                        <span>{val.description}</span>
+                                    </div>
+                                ))
+                                :activityList.slice(0,2).map((val,index)=>(
+                                    <div className="shop-activity-item" key={index}>
+                                        <span className="activity-icon" style={{background:`#${val.icon_color}`}}>{val.icon_name}</span>
+                                        <span>{val.description}</span>
+                                    </div>
+                                ))
+                            }
                         </div>
                         {activityList.length>2
                             ? <div className="activityBtn" onClick={this.toggleShowMore.bind(this)}>
                                 <div>
-                                    <span>3个活动</span>
+                                    <span>{activityList.length}个活动</span>
                                     <i className= {`iconfont icon-sanjiao1 ${showMore?"rotate180":"rotate0"}`} ></i>  
                                 </div>
                             </div>
--- src/components/ShopItem/index.jsx
+++ src/components/ShopItem/index.jsx
@@ -14,31 +14,48 @@ const ShopItem = ({shopData}) => {
                 <div className="shop-info">
                     <div className="shop-title">
                         <h3 className="title">{restaurant.name}</h3>
-                        <ul className="top-right"><li>票</li></ul>
+                        <ul className="top-right">
+                            {restaurant.recommend.is_ad
+                            ?<li className="ad">广告</li>
+                            :""}
+                            {restaurant.supports.length>0
+                            ?restaurant.supports.map((val,index) => (
+                                <li className="support" key={index} style={{color:`#${val.icon_color}`}}>{val.icon_name}</li>
+                            ))
+                            :""}
+                        </ul>
                     </div>
                     <div className="shop-rate">
                         <div className="shop-rate-left">
                             <div className="star"></div>
-                            <div className="grade">{restaurant.rating}</div>
-                            <div className="grade">月售单</div>
+                            <div className="grade">{restaurant.rating}</div>&nbsp;
+                            <div className="grade">月售{restaurant.recent_order_num}单</div>
                         </div>
                         <div className="shop-rate-right">
-                            <div>蜂鸟</div>
+                            {restaurant.delivery_mode
+                                ?<div className="delivery" style={{color:`#${restaurant.delivery_mode.text_color}`,
+                                    background: `linear-gradient(to right, #${restaurant.delivery_mode.gradient.rgb_from}, #${restaurant.delivery_mode.gradient.rgb_to})`
+                                }}>
+                                    {restaurant.delivery_mode.text}
+                                </div>
+                                :""}
                         </div>
                     </div>
                     <div className="distribution-info">
                         <div className="distribution-left">
-                            <span>起送</span>|
-                            <span>配送费</span>
+                            <span>¥{restaurant.float_minimum_order_amount}起送</span>&nbsp;|&nbsp;
+                            <span>配送费¥{restaurant.float_delivery_fee}</span>
                         </div>
                         <div className="distribution-right">
-                            <span>m</span>|
-                            <span>分钟</span>
+                            <span>{restaurant.distance<1000?`${restaurant.distance}m`:`${(restaurant.distance/1000).toFixed(2)}km`}</span>&nbsp;|&nbsp;
+                            <span>{restaurant.order_lead_time}分钟</span>
                         </div>
                     </div>
+                    
+                    
                 </div>
             </div>
-            <ShopActivity/>
+            <ShopActivity activities={restaurant.activities} recommend={restaurant.recommend}/>
         </div>)
 }
 export default ShopItem ;
