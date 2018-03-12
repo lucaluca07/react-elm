c8f733bfdb365fea632b636f9b8618a7d93c7c76
liucannn@sina.com <liucannn@sina.com>
2018-03-12 23:19:28 +0800

    filter

--- src/components/FilterNav/index.jsx
+++ src/components/FilterNav/index.jsx
@@ -8,32 +8,54 @@ export default class FilterNav extends Component{
         console.log(fliterMore)
         return(
             <div className="filter-nav">
-                <div className="filter">
+                <div className="filter-wrap">
                     {delivery_mode&&
-                    <dl>
+                    <dl className="filter-item">
                         <dt className="title">配送方式</dt>
                         <dd className="content">{
-                            <div>
+                            <div className="more-filter">
                                 123
                             </div>
                         }</dd>
                     </dl>}
                     {activity_types.length>0&&
-                    <dl>
+                    <dl className="filter-item">
                         <dt className="title">优惠活动</dt>
                         <dd className="content">{
-                            activity_types.map((val,index) => (<div>2</div>))
+                            activity_types.map((val,index) => (<div key={index} 
+                                style={{borderBottom:index<3&&activity_types.length>3?"1px solid #e4e3e3":"none",
+                                    borderRight:index===2||index===5?"none":"1px solid #e4e3e3"}}
+                                className="more-filter">
+                                <div className="activity-icon" 
+                                    style={{background:`#${val.icon_color}`,}}>{val.icon_name}</div>
+                                <span>{val.name}</span>
+                            </div>))
                         }</dd>
                     </dl>}
                     {average_costs.length>0&&
-                    <dl>
+                    <dl className="filter-item">
                         <dt className="title">人均消费</dt>
-                        <dd className="content">{average_costs.map((val,index) => (<div>2</div>))}</dd>
+                        <dd className="content">{average_costs.map((val,index) => (<div 
+                            key={index}
+                            style={{borderBottom:index<3&&average_costs.length>3?"1px solid #e4e3e3":"none",
+                                borderRight:index===2||index===5?"none":"1px solid #e4e3e3"}}
+                            className="more-filter">
+                            <span className="average-costs">{val.description}</span>
+                            </div>))}
+                        </dd>
                     </dl>}
                     {supports.length>0&&
-                    <dl>
+                    <dl className="filter-item">
                         <dt className="title">商家属性</dt>
-                        <dd className="content">{supports.map((val,index) => (<div>2</div>))}</dd>
+                        <dd className="content">{supports.map((val,index) => (<div key={index} 
+                                style={{borderBottom:index<3&&activity_types.length>3?"1px solid #e4e3e3":"none",
+                                    borderRight:index===2||index===5?"none":"1px solid #e4e3e3"}}
+                                className="more-filter">
+                                <div className="support-icon" 
+                                    style={{color:`#${val.icon_color}`,borderColor:`#${val.icon_color}`}}>{val.icon_name}</div>
+                                <span>{val.name}</span>
+                            </div>))}
+                        </dd>
                     </dl>}
                 </div>
             </div>
