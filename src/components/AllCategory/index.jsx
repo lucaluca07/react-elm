094b40d46683dcad387b0c088cfc2298f909eacb
liucannn@sina.com <liucannn@sina.com>
2018-03-13 22:30:22 +0800

    category

---
+++ src/components/AllCategory/index.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+import './style.scss'
+
+export default class AllCategory extends Component{
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
