7e4e62b7b3c2a60ab9e736a0342c30bd219fd3df
volcanoliuc <liucannn@sina.com>
2018-04-13 18:03:14 +0800

    样式微调

---
+++ src/components/AddAddress/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CSSModules from "react-css-modules";
+import { Link } from "react-router-dom";
+import styles from "./style.scss";
+import Item from "./Item";
+
+const AddAddresses = ({ data = [] }) => (
+  <div>
+    {data.map((address, index) => <Item key={index} address={address} />)}
+    <Link to={"/my/address/add"}>
+      <div styleName="add">
+        <i className="iconfont icon-jia" />
+        <span>新增收货地址</span>
+      </div>
+    </Link>
+  </div>
+);
+
+export default CSSModules(AddAddresses, styles, { allowMultiple: true });
